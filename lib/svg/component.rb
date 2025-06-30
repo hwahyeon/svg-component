df45b03885d1318023861365daf197459c909fe3
Janghyeon <hwahyeon.dev@gmail.com>
2025-06-30 15:14:46 +0200

    chore: Initialize project structure

---
+++ lib/svg/component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "component/version"
+
+module Svg
+  module Component
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
