68d22678f1bc0285239e4e2de53940527598d078
Janghyeon <hwahyeon.dev@gmail.com>
2025-06-30 16:04:28 +0200

    feat: Improve attribute handling for SVG render

--- lib/svg/component.rb
+++ lib/svg/component.rb
@@ -9,19 +9,32 @@ module Svg
     # Render an SVG file with optional HTML attributes
     #
     # @param [String] path - The path to the SVG file
-    # @param [Hash] options - Optional HTML attributes (e.g., :class, :title)
-    # @return [String] Rendered SVG content or a fallback comment
+    # @param [Hash] options - Supported options: :class, :title, :width, :height
+    # @return [String] SVG content with applied attributes, or fallback comment
     def self.render(path, options = {})
       return "<!-- SVG file not found -->" unless File.exist?(path)
 
       svg = File.read(path)
 
-      # Add class attribute if provided
-      if options[:class]
-        svg.sub!("<svg", "<svg class=\"#{options[:class]}\"")
+      # Modify <svg ...> tag with new attributes
+      svg.sub!(/<svg([^>]*)>/) do
+        attrs = Regexp.last_match(1)
+
+        # Remove existing class, width, height if replacements provided
+        attrs = attrs.gsub(/\sclass="[^"]*"/, "") if options[:class]
+        attrs = attrs.gsub(/\swidth="[^"]*"/, "") if options[:width]
+        attrs = attrs.gsub(/\sheight="[^"]*"/, "") if options[:height]
+
+        # Build new attributes
+        new_attrs = []
+        new_attrs << "class=\"#{options[:class]}\"" if options[:class]
+        new_attrs << "width=\"#{options[:width]}\"" if options[:width]
+        new_attrs << "height=\"#{options[:height]}\"" if options[:height]
+
+        "<svg#{attrs} #{' ' unless attrs.strip.empty?}#{new_attrs.join(' ')}>"
       end
 
-      # Add title tag for accessibility if provided
+      # Insert <title> tag if provided
       if options[:title]
         svg.sub!(">", "><title>#{options[:title]}</title>")
       end
